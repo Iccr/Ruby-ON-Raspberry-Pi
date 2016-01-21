17a3a634797778b4882287e810a2bc7217e0f6a1
ccr <sis.ccr@gmail.com>
2016-01-21 20:24:49 +0545

    status column added to devices table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118173248) do
+ActiveRecord::Schema.define(version: 20160121143936) do
 
   create_table "devices", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160118173248) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "status"
   end
 
   create_table "users", force: :cascade do |t|
