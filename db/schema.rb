7e968a56b6029c277679e33a9ef01e227551a9f5
ccr <sis.ccr@gmail.com>
2016-02-03 15:36:14 +0545

    relationship in conversation model and message model generated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201131858) do
+ActiveRecord::Schema.define(version: 20160203095103) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20160201131858) do
     t.datetime "updated_at", null: false
     t.boolean  "status"
   end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
